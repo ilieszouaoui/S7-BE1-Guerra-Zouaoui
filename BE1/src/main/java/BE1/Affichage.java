149ebcd420e6326d77c4d3b9fd7ab01134153cdc
zouaoui <ilies.zouaoui@ecl19.ec-lyon.fr>
2020-09-17 11:59:58 +0200

    Création des fichiers d'affichage et d'appel de la fonction principale

---
+++ BE1/src/main/java/BE1/Affichage.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BE1;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author iliès
+ */
+public class Affichage extends JFrame{
+     public Affichage(){
+        setTitle("Jeu Araignée");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # S7-BE1-Guerra-Zouaoui
 Projet de BE1 Araignée - Appro Java S7
+
+Création d'un jeu "d'arraignée"
